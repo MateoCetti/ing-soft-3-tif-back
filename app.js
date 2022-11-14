3886e2210da976bd369ddd44b44b8d5658f94eb7
Mateo P. Cetti <mateocetti2000@gmail.com>
2022-11-14 18:03:50 -0300

    Add get pokemon request on root endpoint

---
+++ app.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require('mongodb')
+const express = require('express')
+
+var cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+
+const port = 3030
+const uri =
+  "mongodb://root:password@0.0.0.0:27017/";
+
+const client = new MongoClient(uri);
+
+app.get('/', async (req, res) => {
+  let pokemon = await client.db("pokemon_db").collection("pokemon_collection").findOne({"name": req.query.name});
+  if (pokemon == null){
+    res.send({error:"No pokemon found"});
+    return;
+  }
+  console.log(pokemon.name)
+  res.send({name: pokemon.name, 
+    types: [pokemon.type1, pokemon.type2]})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
