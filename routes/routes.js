764bf6f190fd3d71c910bbc20f63734a2b9c7b70
Mateo P. Cetti <mateocetti2000@gmail.com>
2022-12-01 10:30:04 -0300

    Make request pokemon name case insensitive

--- routes/routes.js
+++ routes/routes.js
@@ -1,57 +1,57 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import { mongo_fetch_pokemon } from '../database/mongo.js'
+import { mongo_fetch_pokemon } from "../database/mongo.js";
 
 const pokemon_router = Router();
 
+async function get_pokemon(req, res) {
+  const name = fix_name(req.query.name);
+  const number = req.query.number;
+  const pokemon = await mongo_fetch_pokemon(name != undefined ? name : number);
+  if (pokemon == null) {
+    res.send({ error: "Pokemon not found." });
+    return;
+  }
 
-async function get_pokemon(req, res){
-    const name = req.query.name;
-    const number = req.query.number;
-    const pokemon = await mongo_fetch_pokemon(name != undefined ? name : number);  
-    if (pokemon == null) {
-        res.send({error:"Pokemon not found."});
-        return;
-    }
-  
-    // Make pokedex number a three digit string
-    pokemon.pokedex_number = fix_pokemon_number(pokemon.pokedex_number);
-    pokemon.types = fix_types(pokemon);
-    
-    res.send(
-      {
-        name: pokemon.name,
-        number: pokemon.pokedex_number, 
-        types: pokemon.types,
-        weight: pokemon.weight_kg,
-        generation: pokemon.generation,
-        is_legendary: pokemon.is_legendary,
-        japanese_name: pokemon.japanese_name
+  // Make pokedex number a three digit string
+  pokemon.pokedex_number = fix_pokemon_number(pokemon.pokedex_number);
+  pokemon.types = fix_types(pokemon);
 
-      }
-    );
-}
-  
-  
-export function fix_pokemon_number(number){
-    number = String(number)
-    while (number.length !== 3){
-      number = '0'+ number;
-    }
-    
-    return number;
+  res.send({
+    name: pokemon.name,
+    number: pokemon.pokedex_number,
+    types: pokemon.types,
+    weight: pokemon.weight_kg,
+    generation: pokemon.generation,
+    is_legendary: pokemon.is_legendary,
+    japanese_name: pokemon.japanese_name,
+  });
 }
 
+export function fix_name(name) {
+  // converting first letter to uppercase
+  const capitalized = name.charAt(0).toUpperCase() + name.slice(1);
 
-export function fix_types(pokemon){
-    const types = [pokemon.type1]
-    if (pokemon.type2 == "") return types;
-    
-    types.push(pokemon.type2);
-    return types;
+  return capitalized;
 }
 
+export function fix_pokemon_number(number) {
+  number = String(number);
+  while (number.length !== 3) {
+    number = "0" + number;
+  }
 
-pokemon_router.get('/', get_pokemon);
+  return number;
+}
+
+export function fix_types(pokemon) {
+  const types = [pokemon.type1];
+  if (pokemon.type2 == "") return types;
+
+  types.push(pokemon.type2);
+  return types;
+}
+
+pokemon_router.get("/", get_pokemon);
 
 export default pokemon_router;
--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,4 +1,4 @@
-import {fix_types, fix_pokemon_number} from '../routes/routes.js'
+import {fix_types, fix_pokemon_number, fix_name} from '../routes/routes.js'
 
 const pokemon_numbers = [
     [1, "001"],
@@ -10,7 +10,21 @@ const pokemon_numbers = [
     [0, "000"]
 ]
 
+const pokemon_names = [
+    ["charizard","Charizard"],
+    ["mew","Mew"],
+    ["Charmander","Charmander"],
+    ["gardevoir","Gardevoir"],
+    ["mewtwo","Mewtwo"],
+]
+
 test.each(pokemon_numbers)('Test pokemon numbers', async (provided_number, expected_number) => {
-    const fixed_number = fix_pokemon_number(provided_number)
+    const fixed_number = fix_pokemon_number(provided_number);
     expect(fixed_number).toBe(expected_number);
 });
+
+
+test.each(pokemon_names)("Test pokemon names", async (provided_name, expected_name) => {
+    const fixed_name = fix_name(provided_name);
+    expect(fixed_name).toBe(expected_name)
+})
